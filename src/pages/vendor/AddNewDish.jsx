53bbc7c1aed85b7c8c554e67a4c97db79be35f75
minhduc <minhducdl1010@gmail.com>
2023-10-19 20:13:14 +0700

    upload new menu

--- src/pages/vendor/AddNewDish.jsx
+++ src/pages/vendor/AddNewDish.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import { BiArrowBack } from "react-icons/bi"
 import { useNavigate } from "react-router-dom"
 
@@ -8,14 +8,34 @@ const AddNewDish = () => {
 	const priceRef = useRef(null)
 	const categoryRef = useRef(null)
 	const imageRef = useRef(null)
+	const [selectedImg, setSelectedImg] = useState(null)
 
-	function handleAddNewDish() {
+	async function handleAddNewDish() {
 		const name = nameRef.current.value
 		const price = priceRef.current.value
 		const category = categoryRef.current.value
 
-		if (!name || !price) return
-		console.log("test")
+		if (!name || !price || !selectedImg) return
+
+		const formData = new FormData()
+		formData.append("name", name)
+		formData.append("price", price)
+		formData.append("category", category)
+		formData.append("image", selectedImg)
+
+		fetch("/upload", {
+			method: "POST",
+			body: formData,
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				// Handle the response from the server
+				console.log(data)
+			})
+			.catch((error) => {
+				// Handle errors
+				console.error(error)
+			})
 	}
 
 	return (
@@ -59,13 +79,24 @@ const AddNewDish = () => {
 					<label htmlFor="file" className="mb-1 text-gray-700 block font-semibold">
 						Image
 					</label>
-					<input type="file" className="hidden" id="file" ref={imageRef} />
+					<input
+						type="file"
+						className="hidden"
+						id="file"
+						ref={imageRef}
+						onChange={(e) => {
+							setSelectedImg(e.target?.files[0])
+						}}
+					/>
 					<div
-						className="w-full h-[6rem] bg-green-50 rounded-xl border-[2px] border-dashed border-green-400 flex items-center justify-center text-green-300"
+						className="w-full h-[6rem] bg-green-50 rounded-xl border-[2px] border-dashed border-green-400 flex items-center justify-center text-green-300 overflow-hidden"
 						onClick={() => {
 							imageRef.current.click()
 						}}>
-						Upload the food's image
+						{selectedImg ? selectedImg.name : "Upload the food's image"}
+						{selectedImg && (
+							<img src={URL.createObjectURL(selectedImg)} alt="" className="w-[3rem] h-[3rem] ml-2" />
+						)}
 					</div>
 				</div>
 			</div>
